101d33a58bd01f6a85d9b329af4fe340cbb62d71
suhaib927 <liosu927@gmail.com>
2023-05-09 21:03:28 +0300

    giriş kontrol yapılmıştır.

---
+++ php/giris.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/bootstrap.css">
+    <link rel="stylesheet" href="../css/styles.css">
+</head>
+<body class="bg-primary text-white text-center" >
+    <div class="container mx-auto bg-primary">
+        <div class="row fs-3 text-center text-white">
+            <div class="col-sm-12">
+            <?php
+                $email = $_POST['username'];
+                $password = $_POST['password'];
+                $pos =strpos($email, '@');
+                
+                if ($pos !== false) {
+                   
+                    $username = substr($email, 0, $pos);
+                }
+                if($username == $password)
+                {
+                   echo "<br><br><br><br><br><br><br>HOŞGELDİNİZ \"". strtoupper($username). "\"<br>";
+                   echo "<b>5 sn hakkında sayfasına yöneleceksiniz</b>";
+                   header("refresh: 5; url = ../index.html"); 
+                }
+                else
+                {
+                    echo "<br><br><br><br><br><br><br>!!!şifreniz hatalıdır!!!<br>";
+                    echo "!!!şifreniz, mail adresinizin ilk kısmı ile aynı olmalıdır!!!<br>";
+                    echo "<b>3 saniye içerisinde giriş sayfasına yöneleceksiniz...</b>";
+                    header("refresh: 3; url = ../html/giris.html");
+                }
+            ?>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
